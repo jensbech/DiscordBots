2743f629b42c13bd903146d722ef1d00481f522f
Jens Bech-Sørensen <jens@bechsor.no>
2025-02-16 15:22:47 +0100

    initial roller

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import { BoredBot } from "./discord";
 // const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 // const token = process.env.BORED_BOT_DISCORD_TOKEN as string;
-const token =
-	"MTM0MDY3MjY5MzA4Mjc4Mzg0OQ.Gtw0Xb.WrVud3xZBC4JcTpyiLMhRP-AEzPtr3q_05jDEg";
+const token = "";
 const bot = new BoredBot();
 
 await bot.start(token);

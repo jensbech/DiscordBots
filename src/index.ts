47e021fca70346eb58986def1c6d3a7bd3059c26
Jens Bech-Sorensen <jens@bechsor.no>
2025-02-23 19:01:33 +0000

    env

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,5 @@ import { Elysia } from "elysia";
 import { BoredBot } from "./boredBot";
 import { boredBotCommands } from "./boredBot/commands";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
-const token = "";
-const applicationId = "";
-
-await BoredBot.getInstance(token, applicationId, boredBotCommands);
+new Elysia().get("/", () => "Hello Elysia").listen(3000);
+await BoredBot.getInstance(process.env.TOKEN, process.env.APPLICATION_ID, boredBotCommands);

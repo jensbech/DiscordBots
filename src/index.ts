71f89f27f2dbc81ba54a3e64c4421a0c0a5a6b0d
Jens Bech-Sørensen <jens@bechsor.no>
2025-04-17 01:04:50 +0200

    Simplify environment setup by leveraging bitwarden secrets manager

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import { config } from "dotenv";
-config();
-
-import { Elysia } from "elysia";
+// import { Elysia } from "elysia";
 import { BoredBot } from "./boredBot";
 import { boredBotCommands } from "./boredBot/commands";
 
-new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// new Elysia().get("/", () => "Hello Elysia").listen(3000);
 if (!process.env.DISCORD_BOT_TOKEN || !process.env.APPLICATION_ID) {
 	throw new Error("DISCORD_BOT_TOKEN and APPLICATION_ID must be defined in the environment variables.");
 }
